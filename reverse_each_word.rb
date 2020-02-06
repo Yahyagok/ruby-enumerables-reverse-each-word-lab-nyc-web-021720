3df5c263743094d108d784d4e02fe8b7b537a712
Yahya  Gok <ygok17@hotmail.com>
2020-02-06 03:40:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   
 
-  original_array = string.split(" ")
+  array = string.split(" ")
   return_array = []
   original_array.each do|x|
     return_array << x.reverse
